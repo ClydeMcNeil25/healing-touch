1c2185d8dddf0786084d2b5f8b93d3229cc9c950
CMcNeil <108107540+ClydeMcNeil25@users.noreply.github.com>
2022-09-25 09:16:19 -0500

    returned header to normal for testing

--- healing-touch/src/App.js
+++ healing-touch/src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <h1>Daniel's Healing with a touch Touch</h1>
+      <h1>Daniel's Healing Touch</h1>
     </div>
   );
 }
