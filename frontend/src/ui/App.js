f66c55c60d48c0948499f623efdcd9c1eb89be8e
CMcNeil <108107540+ClydeMcNeil25@users.noreply.github.com>
2022-09-26 10:04:34 -0500

    added style css

--- frontend/src/ui/App.js
+++ frontend/src/ui/App.js
@@ -4,6 +4,7 @@ import { Home } from './Home'
 import { FourOhFour } from './FourOhFour'
 import React from 'react'
 import {Navigation} from './Navigation'
+import './styles/style.css'
 
 export const App = () => (
 	<>
--- frontend/src/ui/Home.js
+++ frontend/src/ui/Home.js
@@ -3,7 +3,7 @@ import React from "react"
 export const Home = () => {
    return (
       <>
-         <h1>Welcome to Daniel's Healing Touch</h1>
+         <h1 className="text-white">Welcome to Daniel's Healing Touch</h1>
       </>
    )
 }
